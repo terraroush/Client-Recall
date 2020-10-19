3846526609b0565fa6037d22cc8d48ad0c2c9ae9
Terra Roush <terra.roush@gmail.com>
2020-10-19 11:37:48 -0500

    added get, add, delete, edit, getById for clients in clientProvider.js

---
+++ src/clients/ClientProvider.js
@@ -0,0 +1,56 @@
+import React, { useState, createContext } from "react"
+
+
+export const ClientContext = createContext()
+
+
+export const ClientProvider = props => {
+    const [clients, setClients] = useState([])
+    const [ searchTerms, setSearchTerms ] = useState("")
+
+    const getClients = () => {
+        return fetch("http://localhost:8088/clients?_expand=user")
+            .then(res => res.json())
+            .then(setClients)
+    }
+
+    const getClientById = id => {
+        return fetch(`http://localhost:8088/clients/${id}?_expand=user`)
+            .then(res => res.json())
+    }
+
+    const addClient = clientObj => {
+        return fetch("http://localhost:8088/clients", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(clientObj)
+        })
+            .then(getClients)
+    }
+    const deleteClient = clientId => {
+        return fetch(`http://localhost:8088/clients/${clientId}`, {
+            method: "DELETE"
+        })
+            .then(getClients)
+    }
+    const editClient = client => {
+        return fetch(`http://localhost:8088/clients/${client.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(client)
+        })
+            .then(getClients)
+    }
+
+    return (
+        <ClientContext.Provider value={{
+            clients, getClients, addClient, getClientById, deleteClient, editClient, searchTerms, setSearchTerms
+        }}>
+            {props.children}
+        </ClientContext.Provider>
+    )
+}

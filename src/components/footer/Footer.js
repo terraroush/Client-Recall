9f3f26e82aa0f8ca82087185f8b148e0878dc09d
Terra Roush <terra.roush@gmail.com>
2020-10-21 14:38:31 -0500

    footer is good

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Footer.css"
+
+export const Footer = () => {
+
+    return (
+        <footer className="footer">
+            <section>copyright 2020 client recall brought to you by terra roush</section>
+        </footer>
+    )
+}

f2505d956dc2de35f1a2ce1f91b7f69789f214dc
Terra Roush <terra.roush@gmail.com>
2020-10-21 12:49:26 -0500

    got logo svg file into header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { NavBar } from "../nav/NavBar"
+import { Logo } from "../logo/Logo"
+import "./Header.css"
 
 export const Header = () => {
 
     return (
         <>
+            <Logo />
             <h1 className="sticky">client recall</h1>
             <NavBar />
         </>

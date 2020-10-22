10508cc3078fd95a6d860b5a218ffbd8fe4da2e4
Terra Roush <terra.roush@gmail.com>
2020-10-22 11:38:54 -0500

    user card displays current username

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -1,2 +1,24 @@
 import React from "react"
+import { Card, Icon, Image } from 'semantic-ui-react'
+import "./UserCard.css"
 
+// const totalClients = 
+const username = localStorage.getItem("username")
+
+export const UserCard = () => (
+  <Card id="user-card">
+    <Image src={'src/images/dummyPics/terraJumpsuit.jpeg'} alt="profile picture" centered size='medium'  wrapped ui={false} />
+    <Card.Content>
+      <Card.Header className="card-header">{username}</Card.Header>
+      <Card.Description>
+        kill them with kindness.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='users' />
+        {} total clients
+      </a>
+    </Card.Content>
+  </Card>
+)

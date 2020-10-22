d3cee724cbd958f1d61c75864b641c6a6af98d7b
Terra Roush <terra.roush@gmail.com>
2020-10-22 17:01:46 -0500

    pause in production, moing on with other features

--- src/components/AppViews.js
+++ src/components/AppViews.js
@@ -64,6 +64,7 @@ export const AppViews = () => {
             <Route exact path="/client-center">
                 <Header />
                 <SidebarLeft />
+                <div></div>
                 <SidebarRight />
                 <Footer />
             </Route>
--- src/components/clients/ClientName.js
+++ src/components/clients/ClientName.js
@@ -1,29 +1,14 @@
-import React, { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import { ClientDetail } from "./ClientDetail";
-import { ClientContext } from "./ClientProvider";
 import "./ClientName.css"
 
 export const ClientName = ({ client }) => {
-    const {clientId} = useParams()
-    const { getClients } = useContext(ClientContext)
-
-    const displayDetail = () => {
-        if (clientId){
-            return <ClientDetail />
-        } else {
-            return <ClientDetail />
-        }
-    }
-    useEffect(() => {
-        getClients()
-      }, []);
 
   return (
     <section className="clientName-card">
       <button
         className="nameButtons"
-        onClick={() => displayDetail()}
+        onClick={() => <ClientDetail />}
         title={client.firstName + " " + client.lastName}>
         {client.firstName + " " + client.lastName}
       </button>

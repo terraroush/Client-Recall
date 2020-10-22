f6f59c3f54d5527e384de5c47724210567b70e64
Terra Roush <terra.roush@gmail.com>
2020-10-22 14:03:12 -0500

    got footer to stay at the bottom

--- src/components/clients/ClientCard.js
+++ src/components/clients/ClientCard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import "./ClientCard.css"
 
 export const ClientCard = ({ client }) => (
-    <section className="client">
-        <h3 className="client__name">
+    <section className="clientCard">
+        <h4 className="client__name">
             <Link to={`/clients/detail/${client.id}`}>{ client.firstName + " " + client.lastName }
             </Link>
-        </h3>
+        </h4>
+        <div className="client__email">{client.email}</div>
+      <div className="client__phone">{client.phone}</div>
     </section>
 
 )

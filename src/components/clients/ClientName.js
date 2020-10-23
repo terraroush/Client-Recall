e97dc83072383a613eafaa2276c557fdae817a6b
Terra Roush <terra.roush@gmail.com>
2020-10-22 21:40:58 -0500

    modal is poppin
    q

--- src/components/clients/ClientName.js
+++ src/components/clients/ClientName.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { ClientDetail } from "./ClientDetail";
+import React, { useRef } from "react";
+import Modal from "../modals/Modal"
+// import { ClientDetail } from "./ClientDetail";
 import "./ClientName.css"
 
 export const ClientName = ({ client }) => {
+  const modal = useRef(null)
 
   return (
+    <>
     <section className="clientName-card">
       <button
         className="nameButtons"
-        onClick={() => <ClientDetail />}
+        onClick={() => modal.current.open()}
         title={client.firstName + " " + client.lastName}>
         {client.firstName + " " + client.lastName}
       </button>
-      
+      <Modal ref={modal}>Did it work?</Modal>
     </section>
+    </>
   );
 };
--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,20 +1,22 @@
 import React, { useState, useImperativeHandle, forwardRef } from "react"
 import { createPortal } from "react-dom"
-import { Modal } from "semantic-ui-react"
 import "./Modal.css"
 
 const modalElement = document.getElementById("modal-root")
 
-export const Modal = ({ children, defaultOpened = false }, ref) => {
+export function Modal({ children, fade = false, defaultOpened = false }, ref) {
     const [ isOpen, setIsOpen ] = useState(defaultOpened)
 
     useImperativeHandle(ref, () => ({
         open: () => setIsOpen(true),
         close: () => setIsOpen(false)
-    }), [close])
+    }), [])
+
 
     return createPortal(
         isOpen ? <div className="modal">{children}</div> : null ,
         modalElement
     )
 }
+
+export default forwardRef(Modal)

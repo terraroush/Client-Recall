4eeb66e05a3286fc8ebae62110ab6d447ec3ca3e
Terra Roush <terra.roush@gmail.com>
2020-10-26 15:38:00 -0500

    brought client card in with visitCard

--- src/components/clients/ClientName.js
+++ src/components/clients/ClientName.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import Modal from "../modals/Modal"
 import { ClientDetail } from "./ClientDetail";
 import "./ClientName.css"
--- src/components/visits/VisitList.js
+++ src/components/visits/VisitList.js
@@ -2,19 +2,33 @@ import React, { useContext, useEffect, useState } from "react"
 import { VisitContext } from "./VisitProvider"
 import { VisitCard } from "./VisitCard";
 import { useParams } from "react-router-dom";
+import { ClientCard } from "../clients/ClientCard";
+import { ClientContext } from "../clients/ClientProvider";
 
 export const VisitList = () => {
   const { visits, getVisitsByClientId } = useContext(VisitContext);
+  const { clients, getClients } = useContext(ClientContext);
   const {clientId} = useParams()
   const activeUser = parseInt(localStorage.getItem("activeUser"));
 
   useEffect(() => {
     getVisitsByClientId(clientId)
+    .then(() => {
+      getClients()
+      
+    })
   }, [])
   
   return (
     <article className="visitList--grid">
       <h1>client history of</h1>
+      <div className="visitList--clientCard">
+        {clients.map(client => {
+          if (client.id === +clientId){
+            return <ClientCard key={client.id} client={client} userId={client.userId} />
+        }})
+        }
+      </div>
       <div className="visitList--list">
         {visits.map((visit) => {
           if (visit.userId === activeUser) {
@@ -25,6 +39,13 @@ export const VisitList = () => {
     </article>
   );
 };
+
+
+
+
+
+
+
 
 
 

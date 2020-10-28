ea85dc5551b09c9c9ffe52fb5b6b30b529bc17c2
Terra Roush <terra.roush@gmail.com>
2020-10-28 14:56:10 -0500

    logout works perfectly. need to rearrange userCard before I can style logout button appropriately

--- src/components/clients/ClientCard.js
+++ src/components/clients/ClientCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useHistory} from "react-router-dom";
-import { VisitList } from "../visits/VisitList";
 import "./ClientCard.css";
 
 export const ClientCard = ({client}) => {
--- src/components/sidebars/SidebarRight.js
+++ src/components/sidebars/SidebarRight.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Logo } from "../logo/Logo"
+import { Logout } from "../logout/Logout";
 import { UserCard } from "../users/UserCard"
 import "./SidebarRight.css";
 
@@ -8,6 +9,9 @@ export const SidebarRight = () => (
     <aside id="sidebar-right" className="sidenav sidebar-right">
       <div id="content">
         <UserCard />
+        <div className="logout-container">
+          <Logout />
+        </div>
         <div className="logo-container">
             <Logo />
         </div>
